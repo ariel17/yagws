c8f0262173fcbe3a48f869438ee59d6f341250e0
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2024-02-20 17:07:11 -0300

    Uses file server from stdlib. Refs #1

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
-package yagws
+package main
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v3"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
+
+type ServerConfig struct {
+	Port          int    `yaml:"port"`
+	ServerPath    string `yaml:"server_path"`
+	StaticContent *struct {
+		RootPath  string `yaml:"root_path"`
+		IndexName string `yaml:"index_name"`
+	} `yaml:"static_content"`
+}
 
 func main() {
 	f, err := os.ReadFile("config.yaml")
@@ -12,15 +24,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var data map[string]interface{}
-
-	err = yaml.Unmarshal(f, &data)
+	serverConfig := ServerConfig{}
+	err = yaml.Unmarshal(f, &serverConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(data)
-	for k, v := range data {
-		log.Println(k, ":", v)
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", serverConfig.Port),
+		Handler:        http.FileServer(http.Dir(serverConfig.StaticContent.RootPath)),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
+	log.Fatal(s.ListenAndServe())
 }
